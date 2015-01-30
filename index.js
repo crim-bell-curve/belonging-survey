a38f0bef3823f22782388cf27d52336e9f142210
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-01-29 21:26:12 -0500

    rest of the charts

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function createRaceChart() {
 		axis: {
 			x: {
 				type: 'category',
-				categories: ['White', 'Black', 'Asian', 'Hispanic', 'Multiracial', 'Other (including Middle Eastern)'],
+				categories: ['White', 'Black', 'Asian', 'Latino', 'Multiracial', 'Other (including Middle Eastern)'],
 			},
 			y2: {
 				show: true
@@ -153,14 +153,138 @@ function createGenderChart() {
 		bindto: '#chart4'
 	})
 }
+function createRaceChart2() {
+	var chart = c3.generate({
+		data: {
+			columns: [
+				['agree',88,65,76,77,82,77],
+				['disagree',3,13,4,7,3,3],
+				['mean', 4.12,3.38,3.68,3.79,3.98,3.82]
+			],
+			type: 'bar',
+			types: {
+				mean: 'line'
+			},
+			axes: {
+				agree: 'y',
+				disagree: 'y',
+				mean: 'y2'
+			}
+		},
+		bar: {
+			width: {
+				ratio: 0.5
+			}
+		},
+		axis: {
+			x: {
+				type: 'category',
+				categories: ['White', 'Black', 'Asian', 'Latino', 'Multiracial', 'Other (include Middle Eastern)']
+			},
+			y2: {
+				show: true
+			}
+		},
+		legend: {
+			position: 'right'
+		},
+		bindto: '#chart5'
+	})
+}
+function createSexOChart2() {
+	var chart = c3.generate({
+		data: {
+			columns: [
+				['agree',85,85,84],
+				['disagree',3,3,0],
+				['mean',4.05,3.92,3.87]
+			],
+			type: 'bar',
+			types: {
+				mean: 'line'
+			},
+			axes: {
+				agree: 'y',
+				disagree: 'y',
+				mean: 'y2'
+			}
+		},
+		bar: {
+			width: {
+				ratio: 0.5
+			}
+		},
+		axis: {
+			x: {
+				type: 'category',
+				categories: ['Heterosexual', 'BGLQ', 'Rather not say']
+			},
+			y2: {
+				show: true
+			}
+		},
+		legend: {
+			position: 'right'
+		},
+		bindto: '#chart6'
+	})
+}
+function createGenderChart2() {
+	var chart = c3.generate({
+		data: {
+			columns: [
+				['agree',83,86],
+				['disagree',4,3],
+				['mean', 3.99,4.06]
+			],
+			type: 'bar',
+			types: {
+				mean: 'line'
+			},
+			axes: {
+				agree: 'y',
+				disagree: 'y',
+				mean: 'y2'
+			}
+		},
+		bar: {
+			width: {
+				ratio: 0.5
+			}
+		},
+		axis: {
+			x: {
+				type: 'category',
+				categories: ['Male', 'Female']
+			},
+			y2: {
+				show: true
+			}
+		},
+		legend: {
+			position: 'right'
+		},
+		bindto: '#chart7'
+	})
+}
 window.addEventListener('load', function(){
-	content = d3.select('#content')
-	content.append('div').attr('id', 'chart1');
-	content.append('div').attr('id', 'chart2');
-	content.append('div').attr('id', 'chart3');
-	content.append('div').attr('id', 'chart4')
+	belonging = d3.select('#belonging')
+
+	belonging.append('div').attr('id', 'chart1');
+	belonging.append('div').attr('id', 'chart2');
+	belonging.append('div').attr('id', 'chart3');
+	belonging.append('div').attr('id', 'chart4');
+
+	nonDisc = d3.select('#nonDisc')
+	nonDisc.append('div').attr('id', 'chart5');
+	nonDisc.append('div').attr('id', 'chart6');
+	nonDisc.append('div').attr('id', 'chart7');
+	
 	createRaceChart();
 	createYearChart();
 	createSexOChart();
 	createGenderChart();
+	createRaceChart2();
+	createSexOChart2();
+	createGenderChart2();
 })
